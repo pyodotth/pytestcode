1858974f30b97ddaf120b648300a8c393b9361c1
pyodotth <pyodotth@gmail.com>
2019-02-04 20:57:20 +0700

    Finish coding

--- charchk.py
+++ charchk.py
@@ -1,5 +1,12 @@
 # TODO check charecter is Upper or Lower
 # use string.istitle()
 
-tmp = "A"
-print(tmp.istitle())
+
+def chkTitle(s):
+    return s.istitle()
+
+
+if __name__ == "__main__":
+    strInput = input("Enter String Check Upper/Lower: ")
+    for i in strInput:
+        print(f"Charecter {i} is Upper : {chkTitle(i)}")
